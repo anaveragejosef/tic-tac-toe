f5fc6c000fd7699b3c53c96197c12a58065feab2
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-08 10:41:55 -0700

    Adds Pseudocode for App
    This commit outlines the three functions that I will begin with. These
    are responsible for tracking moves, checking for a winner, and
    resetting the board.
    
    Also adds a restart button to the index.html.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// Create variable to track turn
+
+// TODO: Create function that updates tiles
+  // Onclick function looking at tile divs
+    // Check if the tile has an inner html that is ''
+      // Change the div to the turn tracker (odd = X, even = 0)
+      // Tracker increments
+    // Call winner checker if turn count >= 5
+
+// TODO: Create function that checks for winner
+  // TODO: Create possible winning board arrays
+
+
+// TODO: Create function that resets game board
+  // Onclick function looking at restart button
+    // On click, iterate through tile divs
+      // Set inner htmls to ''
+    // Reset turn tracker to 1
