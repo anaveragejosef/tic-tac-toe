13abd52ffb3cce5c7ae3d4279fab4625695ae0b2
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-06-08 20:25:35 -0700

    Serves Tic-Tac-Toe App from Server
    This commit allows for the tic-tac-toe game to be displayed on
    localhost:3000 by serving it from the express static function. This
    allows the client to connect to the server.

--- index.js
+++ index.js
@@ -3,35 +3,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Here we are');
-  Score.findAll().then((score) => {
-
-  })
-});
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Server is running and listening on port ${port}`));
 // Create Game State Obj
-var gameState = {
-  xTurnCounter: true,
-  boardSpaces: {
-    topLeft: true,
-    midLeft: true,
-    botLeft: true,
-    topCenter: true,
-    midCenter: true,
-    botCenter: true,
-    topRight: true,
-    midRight: true,
-    botRight: true
-  },
-  scoreboard: {
-    x: 0,
-    o: 0
-  },
-  gameOver: false,
-  winnerSet: false,
-  lastWinner: 'X'
-};
-
-select * from scores,
+// var gameState = {
+//   xTurnCounter: true,
+//   boardSpaces: {
+//     topLeft: true,
+//     midLeft: true,
+//     botLeft: true,
+//     topCenter: true,
+//     midCenter: true,
+//     botCenter: true,
+//     topRight: true,
+//     midRight: true,
+//     botRight: true
+//   },
+//   scoreboard: {
+//     x: 0,
+//     o: 0
+//   },
+//   gameOver: false,
+//   winnerSet: false,
+//   lastWinner: 'X'
+// };
